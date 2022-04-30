c63b4f929b7483b59e5a960b787a0ceb285d19bb
theworm4002 <62223975+theworm4002@users.noreply.github.com>
2022-04-30 00:46:53 -0400

    meow

---
+++ EliteBotConfg.py
@@ -0,0 +1,11 @@
+# Bot Settings
+BNICK   = 'EliteBotDaddy'
+BIDENT  = 'EliteBot'
+BNAME   = 'EliteBot'
+
+BHOME   = '#EliteBot'
+BADMIN  = 'Admin-nick'
+
+# Connection Settings
+BPORT   =  '+6697'
+BSERVER = 'irc.rizon.net'

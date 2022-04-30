18808621dff357b255c2b332739949bf8b271645
theworm4002 <62223975+theworm4002@users.noreply.github.com>
2022-04-29 23:47:49 -0400

    fixed ssl and cleaned up sending to irs

--- elitebot.py
+++ elitebot.py
@@ -1,26 +1,43 @@
 #!/usr/bin/python3
-from config import *
-import socket
-import sys
+
 import ssl
+import socket
+from config import *
  
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if BPORT[0] == '+':
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+if str(BPORT)[:1] == '+':
+    print('Use SSL')
     ircsock = ssl.wrap_socket(ircsock)
+    BPORT = int(BPORT[1:])
+else:
+    BPORT = int(BPORT)
+
 ircsock.settimeout(240)
 
 print(f'Connecting to {BSERVER} :{BPORT}')
+ircsock.connect_ex((BSERVER, BPORT))
+
+def SendIRC(msg):
+    if msg != '': ircsock.send(bytes(f'{msg}\r\n','utf-8'))
+
+def SendMsg(msg, target=BHOME): # Sends messages to the target.
+    SendIRC("PRIVMSG "+ target +" :"+ msg)
  
-ircsock.connect((BSERVER, BPORT))
-ircsock.send(bytes(f'NICK {BNICK}\r\n','utf-8'))
-ircsock.send(bytes(f'USER {BIDENT} 0 * :{BNAME}\r\n','utf-8'))
+SendIRC(f'NICK {BNICK}')
+SendIRC(f'USER {BIDENT} * * :{BNAME}')
 
 while True:
-   recvText = ircsock.recv(2048)
-   text = recvText.decode('utf-8') 
-   line = text.split('\r\n')
-   print(text)
-   if text.find('PING') != -1:
-      pongis = text.split(':')[1]
-      ircsock.send(bytes(f'PONG {pongis}\r\n','utf-8'))
-      ircsock.send(bytes(f'JOIN {BHOME}\r\n','utf8'))
+    recvText = ircsock.recv(2048)
+    text = recvText.decode('utf-8') 
+    line = text.split('\r\n')
+    print(line)
+
+    if text.find('PING') != -1:
+        pongis = text.split(':')[1]
+        SendIRC(f'PONG {pongis}')
+        SendIRC(f'JOIN {BHOME}')
+    
+    if text.find('say hi to') != -1:
+        Nick2TellFkOff = text.split('say hi to ')[1]
+        SendMsg(f'No! Fuck {Nick2TellFkOff}')

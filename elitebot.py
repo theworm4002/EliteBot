cc5a6876103b34314761736633adf700c7d47c24
Colby <60820431+ComputerTech312@users.noreply.github.com>
2022-04-30 02:03:13 +0100

    Fixed pong reply.

--- elitebot.py
+++ elitebot.py
@@ -11,23 +11,26 @@ UseSSL  = True
 
 import socket
 import sys
+import ssl
  
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if usessl:
+if UseSSL:
     ircsock = ssl.wrap_socket(ircsock)
 ircsock.settimeout(240)
 connected = False 
 
 print(f'Connecting to {BSERVER} :{BPORT}')
  
-irc.connect((BSERVER, BPORT))
-irc.send(bytes(f'NICK {BNICK}\r\n','utf-8'))
-irc.send(bytes(f'USER {BIDENT} 0 * :{BNAME}\r\n','utf-8'))
+ircsock.connect((BSERVER, BPORT))
+ircsock.send(bytes(f'NICK {BNICK}\r\n','utf-8'))
+ircsock.send(bytes(f'USER {BIDENT} 0 * :{BNAME}\r\n','utf-8'))
 
 while True:
-   recvText = irc.recv(2048)
+   recvText = ircsock.recv(2048)
    text = recvText.decode('utf-8') 
    line = text.split('\r\n')
    print(text)
    if text.find('PING') != -1:
-      irc.send(bytes(f'PONG {text}[4:]\r\n','utf-8'))
+      pongis = text.split(':')[1]
+      ircsock.send(bytes(f'PONG {pongis}\r\n','utf-8'))
+      ircsock.send(bytes(f'JOIN {BHOME}\r\n','utf-'))

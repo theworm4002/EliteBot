3a13c8fbf0d342b5d101c95e6b6871439cfe86ae
Colby <60820431+ComputerTech312@users.noreply.github.com>
2022-07-11 03:29:21 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# Bot Settings
+
+BNICK   = 'EliteBot'
+BIDENT  = 'EliteBot'
+BNAME   = 'EliteBot'
+BALT    = 'EliteBot-'
+BHOME   = '#technet'
+
+# Connection Settings
+BSERVER = 'irc.technet.chat'
+BPORT   = '+6697' 
+
+# SASL Configuration.
+UseSASL = False
+SANICK  = 'EliteBot'
+SAPASS  = 'password'

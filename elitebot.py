bc7d644c209a770de452b5178bf39f0d7451c426
Colby <60820431+ComputerTech312@users.noreply.github.com>
2022-12-23 07:04:46 +0000

    updated code

--- elitebot.py
+++ elitebot.py
@@ -1,69 +1,95 @@
 #!/usr/bin/python3
- 
-from config import *
+
 import ssl
 import socket
+import time
 import base64
-import random
- 
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+BNICK = 'elitebot'
+BIDENT = 'elitebot'
+BNAME = 'elitebot'
+BSERVER = 'irc.example.net'
+BPORT = '+6697'
+
+SANICK = 'username'
+SAPASS = 'password'
+UseSASL = False
+
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connected = False
 
 def decode(bytes):
-    try: text = bytes.decode('utf-8')
+    try: 
+        text = bytes.decode('utf-8')
     except UnicodeDecodeError:
-        try: text = bytes.decode('latin1')
+        try: 
+            text = bytes.decode('latin1')
         except UnicodeDecodeError:
-            try: text = bytes.decode('iso-8859-1')
+            try: 
+                text = bytes.decode('iso-8859-1')
             except UnicodeDecodeError:
-                text = bytes.decode('cp1252')			
+                text = bytes.decode('cp1252')
     return text
-    
-if str(BPORT)[:1] == '+':
-    print('Use SSL')
-    ircsock = ssl.wrap_socket(ircsock)
-    BPORT = int(BPORT[1:])
-else:
-    BPORT = int(BPORT)
-    
-ircsock.settimeout(240)
- 
-def SendIRC(msg):
-    if msg != '': ircsock.send(bytes(f'{msg}\r\n','utf-8'))
- 
-def SendMsg(msg, target=BHOME):
-    SendIRC("PRIVMSG "+ target +" :"+ msg)
-    
-print(f'Connecting to {BSERVER} :{BPORT}')
-ircsock.connect_ex((BSERVER, BPORT))
-   
-SendIRC(f'NICK {BNICK}')
-SendIRC(f'USER {BIDENT} * * :{BNAME}')
 
-if UseSASL:
-   SendIRC('CAP REQ :sasl')
+def ircsend(msg):
+    if msg != '':
+        ircsock.send(bytes(f'{msg}\r\n','UTF-8'))
 
-   
-while True:
-    recvText = ircsock.recv(2048)
-    ircmsg = decode(recvText)
-    line = ircmsg.strip('\n\r')
-    word = line.split(" ")
-    print(ircmsg)
- 
-    if word[0] == "PING":
-         pongis = ircmsg.split(' ', 1)[1]
-         SendIRC(f'PONG {pongis}')
-            
-    elif line.find('ACK :sasl') != -1 or ircmsg.find('ACK :sasl') != -1:
-       SendIRC('AUTHENTICATE PLAIN')
- 
-    elif ircmsg.find('AUTHENTICATE +') != -1:
-          authpass = SANICK + '\x00' + SANICK + '\x00' + SAPASS
-          ap_encoded = str(base64.b64encode(authpass.encode('UTF-8')), 'UTF-8')
-          SendIRC('AUTHENTICATE ' + ap_encoded)
- 
-    elif ircmsg.find(f' 903 {BNICK} :') != -1:
-       SendIRC('CAP END')
+def connect():
+    global ircsock
+    global connected
+    global BPORT
     
-    elif line.find(f' 001 {BNICK} :') != -1:
-        SendIRC(f'JOIN {BHOME}')
+    ircsock.settimeout(240)
+    
+    if str(BPORT)[:1] == '+':
+        ircsock = ssl.wrap_socket(ircsock)
+        BPORT = int(BPORT[1:])
+    else:
+        BPORT = int(BPORT)
+    
+    ircsock.connect((BSERVER, BPORT))
+    ircsend(f'NICK {BNICK}')
+    ircsend(f'USER {BIDENT} * * :{BNAME}')
+    if UseSASL:
+        ircsend('CAP REQ :sasl')
+    
+def main():
+    global connected
+    if not connected:
+        connect()
+        connected = True
+    
+    while connected:
+        recvText = ircsock.recv(2048)
+        ircmsg = decode(recvText)
+        line = ircmsg.strip('\n\r')
+        print(ircmsg)
+        queued_lines = []
+        if ircmsg.find('PING') != -1:
+            nospoof = ircmsg.split(' ', 1)[1]
+            ircsend("PONG " + nospoof)
+        
+        if line.find('ACK :sasl') != -1 or ircmsg.find('ACK :sasl') != -1:
+            ircsend('AUTHENTICATE PLAIN')
+         
+        elif ircmsg.find('AUTHENTICATE +') != -1:
+            authpass = SANICK + '\x00' + SANICK + '\x00' + SAPASS
+            ap_encoded = str(base64.b64encode(authpass.encode('UTF-8')), 'UTF-8')
+            ircsend('AUTHENTICATE ' + ap_encoded)
+          
+        elif ircmsg.find(f' 903 {BNICK} :') != -1:
+            ircsend('CAP END')
+			
+        if ircmsg.find(f'INVITE {BNICK} :') != -1:
+            channel = line.split(' ')[3]
+            ircsend(f'JOIN {channel}')
+			
+        for line in queued_lines:
+            time.sleep(5)
+            ircsock.send(bytes(f'{line}\r\n','UTF-8'))
+        queued_lines = []
+		
+		if ircmsg.find(f'PRIVMSG') != -1:
+		    text = line.split(' ')[3]
+main()

0cd22a04bd97b6da5ea1768e4a95eab0fb03499b
Colby <60820431+ComputerTech312@users.noreply.github.com>
2023-05-09 05:00:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,24 +6,17 @@ import ssl
 import socket
 import time
 import base64
-import logging
 import sys
-import signal
 from os import path
+from src.logging import Logger
 
 def load_config(config_file):
-    logging.debug("Loading configuration from: %s", config_file)
     with open(config_file, 'r') as file:
         config = json.load(file)
     return config
 
 connected = False
-
-os.makedirs("logs", exist_ok=True)
-logging.basicConfig(level=logging.DEBUG,
-                    filename='logs/elitebot.log',
-                    filemode='w',
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logger = Logger('logs/elitebot.log')
 
 def decode(bytes):
     try: 
@@ -58,7 +51,6 @@ def connect(config):
     if 'BBINDHOST' in config:
         ircsock.bind((config['BBINDHOST'], 0))
 
-    logging.debug("Connecting to server: %s:%s", config['BSERVER'], port)
     ircsock.connect_ex((config['BSERVER'], port))
     ircsend(f'NICK {config["BNICK"]}')
     ircsend(f'USER {config["BIDENT"]} * * :{config["BNAME"]}')
@@ -66,17 +58,13 @@ def connect(config):
        ircsend('CAP REQ :sasl')
 
 def join_saved_channels(config):
-    logging.debug("Joining saved channels")
     
     channels = load_channels()
     
     for channel in channels:
         ircsend(f'JOIN {channel}')
-        if config['ConsoleLogging']:
-            print(f'JOIN {channel}')
 
 def save_channel(channel):
-    logging.debug("Saving channel: %s", channel)
     
     channel = channel.lstrip(':')  # Remove leading colon, if any
     channels = load_channels()
@@ -89,7 +77,6 @@ def save_channel(channel):
             json.dump(channels, f)
 
 def remove_channel(channel):
-    logging.debug("Removing channel: %s", channel)
     
     channel = channel.lstrip(':')  # Remove leading colon, if any
     channels = load_channels()
@@ -112,98 +99,81 @@ def load_channels():
     with open('data/channels.json', 'r') as f:
         return json.load(f)
         
+def parse_message(message):
+    parts = message.split()
+
+    if not parts:
+        return None, None, []
+
+    source = parts[0][1:] if parts[0].startswith(':') else None
+    command = parts[1] if source else parts[0]
+    args_start = 2 if source else 1
+    args = []
+    trailing_arg_start = None
+
+    for i, part in enumerate(parts[args_start:], args_start):
+        if part.startswith(':'):
+            trailing_arg_start = i
+            break
+        else:
+            args.append(part)
+
+    if trailing_arg_start is not None:
+        args.append(' '.join(parts[trailing_arg_start:])[1:])
+
+    return source, command, args
+
+        
 def main(config):
     global connected
 
-    if config.get("ConsoleLogging", False):
-        console = logging.StreamHandler()
-        console.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        console.setFormatter(formatter)
-        logging.getLogger('').addHandler(console)
-        
     while True:
         if not connected:
             try:
                 connect(config)
                 connected = True
             except Exception as e:
-                logging.exception("Error connecting to server: %s", e)
+                logger.error(f"Connection error: {e}")
                 time.sleep(60)
                 continue
 
         try:
             recvText = ircsock.recv(2048)
             if not recvText:
-                logging.warning("Connection closed by server")
                 connected = False
                 continue
 
             ircmsg = decode(recvText)
-            line = ircmsg.strip('\r\n')
-            logging.info(line)
+            source, command, args = parse_message(ircmsg)
+            print(f"Received: source: {source} | command: {command} | args: {args}")
+            logger.debug(f"Received: source: {source} | command: {command} | args: {args}")
 
-            if ircmsg.startswith('PING :') or (ircmsg.find('PING :') != -1 and ircmsg.lower().find('must') == -1):
-                nospoof = ircmsg.split('PING :', 1)[1]
-                if nospoof.find(' ') != -1: nospoof = nospoof.split()[0]
+            if command == "PING":
+                nospoof = args[0][1:] if args[0].startswith(':') else args[0]
                 ircsend(f'PONG :{nospoof}')
-                
-            if line.find('ACK :sasl') != -1 or ircmsg.find('ACK :sasl') != -1:
-               ircsend('AUTHENTICATE PLAIN')
-         
-            elif ircmsg.find('AUTHENTICATE +') != -1:
-               authpass = config["SANICK"] + '\x00' + config["SANICK"] + '\x00' + config["SAPASS"]
-               ap_encoded = str(base64.b64encode(authpass.encode('UTF-8')), 'UTF-8')
-               ircsend('AUTHENTICATE ' + ap_encoded)
 
-            elif ircmsg.find(f' 903 {config["BNICK"]} :') != -1:
-               ircsend('CAP END')
+            if command == 'CAP' and args[1] == 'ACK' and 'sasl' in args[2]:
+                ircsend('AUTHENTICATE PLAIN')
 
-            if ircmsg.find(f':!quit') != -1:
-                ircsend(f'QUIT :Goodbye!')
-                connected = False
-                break
+            elif command == 'AUTHENTICATE' and args[0] == '+':
+                authpass = config["SANICK"] + '\x00' + config["SANICK"] + '\x00' + config["SAPASS"]
+                ap_encoded = str(base64.b64encode(authpass.encode('UTF-8')), 'UTF-8')
+                ircsend('AUTHENTICATE ' + ap_encoded)
 
-            if ircmsg.find(f'INVITE {config["BNICK"]} :') != -1:
-                channel = line.split(' ')[3]
-                ircsend(f'JOIN {channel}')
-                save_channel(channel)
+            elif command == '903':
+                ircsend('CAP END')
 
-            if ircmsg.find(f' 001 {config["BNICK"]} :') != -1:
+            if command == 'PRIVMSG':
+               channel, message = args[0], args[1]
+
+               if message.startswith('!moo'):
+                  ircsend(f'PRIVMSG {channel} :moo')
+
+            if command == '001':
                 join_saved_channels(config)
 
-            if ircmsg.find(f':!moo') != -1:
-                channel = ircmsg.split(' ')[2]
-                ircsend(f'PRIVMSG {channel} :moo')
-
-            if ircmsg.find(f':!join ') != -1:  # Add a space after the !join command
-                channel = ircmsg.split(' ')[4]  # Change the index to 4 to get the channel name
-                ircsend(f'JOIN {channel}')
-                save_channel(channel)
-
-            if ircmsg.find(f':!part') != -1:
-                parts = ircmsg.split(' ')
-                if len(parts) >= 4:  # If the command has a channel name
-                    channel = parts[4]  # Change the index to 4 to get the channel name
-                else:  # If the command does not have a channel name
-                    channel = ircmsg.split(' ')[2]  # Use the channel the command was sent in
-                ircsend(f'PART {channel}')
-                remove_channel(channel)
-
-            if ircmsg.find(f':!restart') != -1:
-                ircsend(f'QUIT :Restarting...')
-                connected = False
-                ircsock.close()
-                os.execv(sys.executable, ['python'] + sys.argv)
-            
-            if ircmsg.find(f':!restart') != -1:
-                ircsend(f'QUIT :Restarting...')
-                connected = False
-                time.sleep(2)
-                os.execv(sys.executable, ["python"] + sys.argv)
-
         except Exception as e:
-            logging.exception("Unexpected error occurred: %s", e)
+            logger.error(f"Error: {e}")
             connected = False
 
     ircsock.close()
